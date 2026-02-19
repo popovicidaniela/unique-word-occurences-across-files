af0ea1958328bf56d434158ed91358b85d1a5cd1
Daniela <popovicidaniela@users.noreply.github.com>
2026-02-19 23:38:47 +0000

    compute sum once in constructor in WordCountResult

--- WordCountResult.cs
+++ WordCountResult.cs
@@ -3,15 +3,18 @@ using System.Linq;
 
 public sealed class WordCountResult
 {
+    private readonly long _totalWordOccurrences;
+
     public IReadOnlyDictionary<string, long> Counts { get; }
     public IReadOnlyList<string> ProcessingErrors { get; }
 
     public int UniqueWordCount => Counts.Count;
-    public long TotalWordOccurrences => Counts.Values.Sum();
+    public long TotalWordOccurrences => _totalWordOccurrences;
 
     public WordCountResult(IReadOnlyDictionary<string, long> counts, IReadOnlyList<string> processingErrors)
     {
         Counts = counts;
         ProcessingErrors = processingErrors;
+        _totalWordOccurrences = counts.Values.Sum();
     }
 }
